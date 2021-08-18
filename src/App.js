47af0c02a1276b1959e2edb1c85243b9453ae8a9
PetiurAliona <alena.petyur@gmail.com>
2021-08-18 20:52:55 +0300

    +1sr task

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import user from "./data/user.json"
+
+import Profile from "./components/Profile/Profile"
+
+const App = () => {
+  return (
+    <>
+      <Profile name={user.name} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,47 @@
+import styles from "./Profile.module.css"
+
+const Profile = ({ name, tag, location, avatar, stats: { followers, views, likes } }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="Аватар пользователя" className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className="">{tag}</p>
+        <p className="">{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+// Profile.defaultProps = {
+//   avatar: defaultImage,
+// }
+
+// Profile.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   avatar: PropTypes.string,
+//   stats: PropTypes.shape({
+//     followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   }).isRequired,
+// }
+
+export default Profile
