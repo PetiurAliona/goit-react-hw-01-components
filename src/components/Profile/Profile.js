68a1502b673f6e67c36003ec16dde3efec4b2110
PetiurAliona <alena.petyur@gmail.com>
2021-08-18 20:56:49 +0300

    +propTypes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import styles from "./Profile.module.css"
+import PropTypes from "prop-types"
 
 const Profile = ({ name, tag, location, avatar, stats: { followers, views, likes } }) => {
   return (
@@ -28,20 +29,16 @@ const Profile = ({ name, tag, location, avatar, stats: { followers, views, likes
   )
 }
 
-// Profile.defaultProps = {
-//   avatar: defaultImage,
-// }
-
-// Profile.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string,
-//   stats: PropTypes.shape({
-//     followers: PropTypes.number.isRequired,
-//     views: PropTypes.number.isRequired,
-//     likes: PropTypes.number.isRequired,
-//   }).isRequired,
-// }
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+}
 
 export default Profile
